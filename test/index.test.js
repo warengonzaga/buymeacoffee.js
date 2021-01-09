01aa00268c5db49a8acb8c2eb25f68e60652f4e7
ty08 <Shreyas.Babu@tesco.com>
2021-01-09 16:17:58 +0530

    Add failure tests

--- test/index.test.js
+++ test/index.test.js
@@ -24,7 +24,24 @@ test('Supporters retreival', async () => {
   const supportersData = await BMCInstance.Supporters();
     expect(supportersData).toStrictEqual(supportersTemplate);
 });
-
+test('Supporters retreival failure', async () => {
+  moxios.stubRequest('supporters', {
+    status: 400,
+    response: supportersTemplate
+  })
+  const BMCInstance = new BMC(token);
+  await expect(BMCInstance.Supporters()).rejects.toThrow();
+});
+test('Supporters retreival: no data', async () => {
+  const BMCResponse = { error: 'No supporters' };
+  moxios.stubRequest('supporters', {
+    status: 200,
+    response: BMCResponse
+  })
+  const BMCInstance = new BMC(token);
+  const supportersData = await BMCInstance.Supporters();
+  expect(supportersData).toStrictEqual(BMCResponse);
+});
 
 test('Subscription retrieval', async () => {
   moxios.stubRequest('subscriptions', {
@@ -35,7 +52,24 @@ test('Subscription retrieval', async () => {
   const subscriptions = await BMCInstance.Subscriptions();
     expect(subscriptions).toStrictEqual(subscriptionsTemplate);
 });
-
+test('Subscription retreival failure', async () => {
+  moxios.stubRequest('subscriptions', {
+    status: 400,
+    response: subscriptionsTemplate
+  })
+  const BMCInstance = new BMC(token);
+  await expect(BMCInstance.Subscriptions()).rejects.toThrow();
+});
+test('Subscription retreival: no data', async () => {
+  const BMCResponse = { error: 'No subscriptions' };
+  moxios.stubRequest('subscriptions', {
+    status: 200,
+    response: BMCResponse
+  })
+  const BMCInstance = new BMC(token);
+  const subscriptions = await BMCInstance.Subscriptions();
+  expect(subscriptions).toStrictEqual(BMCResponse);
+});
 test('Extras retrieval', async () => {
   moxios.stubRequest('extras', {
     status: 200,
